93a48812fec731b1d95ee57856fbab0a5220b75f
Miguel Lucena Contreras <mlcontreras@ingenia.es>
2022-07-13 10:38:04 +0200

    Add Prettier

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-
 import Board from "./lib/Kanbam/Board.svelte";
 </script>
 
-<main class="flex flex-col items-center h-screen m-auto md:max-w-1280 sm:max-w-720">
+<main
+  class="flex flex-col items-center h-screen m-auto md:max-w-1280 sm:max-w-720">
   <h1 class="text-4xl mb-2 mt-4">TODOs</h1>
-  <div class="mb-4 w-1/3 border border-b-gray-400 h-0.5" ></div>
+  <div class="mb-4 w-1/3 border border-b-gray-400 h-0.5"></div>
   <Board />
 </main>
--- src/lib/Kanbam/Panel.svelte
+++ src/lib/Kanbam/Panel.svelte
@@ -1,39 +1,54 @@
 <svelte:options immutable />
+
 <script lang="ts">
-	import { fade } from "svelte/transition"
-	import Todo from "./Todo.svelte";
+import { fade } from "svelte/transition";
+import Todo from "./Todo.svelte";
 
-	export let title: string = "Pending"
+export let title: string = "Pending";
 
-	let tasks : { text: string, id: string }[] = []
-	let newTask : string = ""
+let tasks: { text: string; id: string }[] = [];
+let newTask: string = "";
 
-	function handlerOnDelete( id: string){
-		tasks = tasks.filter( el => el.id !== id)
-	}
+function handlerOnDelete(id: string) {
+  tasks = tasks.filter((el) => el.id !== id);
+}
 
-	const ENTER_KEY = 'Enter'
-	function handlerOnKeyDown( event : KeyboardEvent ){
-		if(event.key === ENTER_KEY && newTask.length > 0){
-			tasks = [...tasks, {
-				text: newTask,
-				id: window.crypto.randomUUID()
-			}]
-			newTask = ""
-		}
-	}
+const ENTER_KEY = "Enter";
+function handlerOnKeyDown(event: KeyboardEvent) {
+  if (event.key === ENTER_KEY && newTask.length > 0) {
+    tasks = [
+      ...tasks,
+      {
+        text: newTask,
+        id: window.crypto.randomUUID(),
+      },
+    ];
+    newTask = "";
+  }
+}
 </script>
 
 <div class="flex flex-col p-5 border border-dashed rounded-md border-gray-800">
-    <h1 class="text-xl text-center mb-2 font-bold">{ title }</h1>
-    {#each tasks as task (task.id)}
-        <Todo bind:task={task.text} on:click={() => handlerOnDelete(task.id)} />
-    {:else}
-        <p in:fade out:fade={{ duration: 250 }} class="mx-auto italic text-sm text-gray-500">No tasks left on this panel</p>
-    {/each}
-    <div class="my-4 border border-b-gray-400 h-0.5"></div>
-    <div class="flex flex-col my-6">
-        <label class="mx-2 text-gray-500 text-xs" for="newTask" >Add new Task</label>
-        <input id="newTask" name="newTask" on:keydown={handlerOnKeyDown} bind:value={newTask} class="m-2 p-2 border border-gray-300 border-b-black border-b-2" type="text"/>
-    </div>
+  <h1 class="text-xl text-center mb-2 font-bold">{title}</h1>
+  {#each tasks as task (task.id)}
+    <Todo bind:task="{task.text}" on:click="{() => handlerOnDelete(task.id)}" />
+  {:else}
+    <p
+      in:fade
+      out:fade="{{ duration: 250 }}"
+      class="mx-auto italic text-sm text-gray-500">
+      No tasks left on this panel
+    </p>
+  {/each}
+  <div class="my-4 border border-b-gray-400 h-0.5"></div>
+  <div class="flex flex-col my-6">
+    <label class="mx-2 text-gray-500 text-xs" for="newTask">Add new Task</label>
+    <input
+      id="newTask"
+      name="newTask"
+      on:keydown="{handlerOnKeyDown}"
+      bind:value="{newTask}"
+      class="m-2 p-2 border border-gray-300 border-b-black border-b-2"
+      type="text" />
+  </div>
 </div>
